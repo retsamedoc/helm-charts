9cadf4c53cedde8fbedd426c6716cf6db154be01
Zach Schimke <retsamedoc@digitalactionsproject.org>
2023-03-25 11:33:30 -0700

    Fix: GH-Actions updates (#2)
    * Fix: Correct repository branch name.
    
    * Fix: Removed cruft leftover from k8s-at-home.
    
    * Fix: Added corrected markdown templates for weatherflow2mqtt

---
+++ .github/scripts/gen-chart-summary.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+# require yq
+command -v yq >/dev/null 2>&1 || {
+    echo >&2 "yq (https://github.com/mikefarah/yq) is not installed. Aborting."
+    exit 1
+}
+
+# Absolute path of repository
+repository=$(git rev-parse --show-toplevel)
+charts_folder="${repository}/charts"
+charts_summary_file="${charts_folder}"/README.md
+
+# Gather all charts using the common library, excluding common-test
+charts=$(find "${charts_folder}" -name "Chart.yaml" | sort)
+
+echo "# Helm charts overview" > "${charts_summary_file}"
+
+echo "| Chart | Description |" >> "${charts_summary_file}"
+echo "| ----- | ----------- |" >> "${charts_summary_file}"
+for i in ${stable_charts[@]}
+do
+    chart_data=($(yq eval '.name, .description' "$i"))
+    chart_name="${chart_data[0]}"
+    chart_description="${chart_data[@]:1}"
+    echo "| [${chart_name}]/${chart_name}) | ${chart_description} |" >> "${charts_summary_file}"
+done
